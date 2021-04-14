51897d26ccf43cf677baf4320ca9ef01fbe38008
migue <pg42877@alunos.uminho.pt>
2021-04-14 22:46:35 +0100

    ...

--- EAMotifs.py
+++ EAMotifs.py
@@ -29,51 +29,63 @@ class EAMotifsInt (EvolAlgorithm):
         maxvalue = self.motifs.seqSize(0) - self.motifs.motifSize
         self.popul = PopulInt(self.popsize, indsize,
                               maxvalue, [])
-
-    def evaluate(self, indivs):
+    
+    def evaluate(self, indivs):#mudar a funcao de avalicao -> usamos o score
         for i in range(len(indivs)):
-            ind = indivs[i]
+            ind = indivs[i]#cada vetor de posicoes
             sol = ind.getGenes()
-            fit = self.motifs.score(sol)
+            fit = self.motifs.score(sol)#avaliar o score que sera a fit para cada vetor de posicoes iniciais
             ind.setFitness(fit)
 
 
-class EAMotifsReal(EvolAlgorithm):
+class EAMotifsReal (EvolAlgorithm):
     def __init__(self, popsize, numits, noffspring, filename):
         self.motifs = MotifFinding()
         self.motifs.readFile(filename, "dna")
-        indsize = len(self.motifs.motifSize) * len(self.motifs.alphabet)
+        indsize = self.motifs.motifSize * len(self.motifs.alphabet)
         EvolAlgorithm.__init__(self, popsize, numits, noffspring, indsize)
 
     def initPopul(self, indsize):
         maxvalue = self.motifs.seqSize(0) - self.motifs.motifSize
-        self.popul = PopulInt(self.popsize, indsize,
+        self.popul = PopulReal(self.popsize, indsize,
                               maxvalue, [])
 
-    def vec_to_pwn(self, v):
-        pwm = createMatZeros(len(self.motifs.alphabet), self.motifs.motifSize)
-        for i in range(0, len(v),len(self.motifs.alphabet)):
-            col_idx = i / len(self.motifs.alphabet)
-            col = v[i:i+len(self.motifs.alphabet)]
+    def vec_to_pwm(self,v):
+        n_alph = len(self.motifs.alphabet)
+        n_motif = self.motifs.motifSize
+
+        pwm = createMatZeros(len(n_alph),n_motif)
+
+        for i in range(0,len(v),n_alph): #
+            col_idx = i / n_alph
+            col = v[i:i+n_alph]
             soma = sum(col)
-            for j in range(len(self.motifs.alphabet)):
-                pwm[i][col_idx] = col[j] / soma
+            for j in range(n_alph):
+                pwm[j][col_idx] = col[j] / soma
 
+        #EXEMPLO:
+        #[1,2,3,4,5,6,7,8,9,10,....]
 
-
+        #A 1
+        #C 2
+        #T 3
+        #G 4
+        
+        #A 1 2 3 4
+        #C
+    
     def evaluate(self, indivs):
         for i in range(len(indivs)):
             ind = indivs[i]
             sol = ind.getGenes()
-            self.motifs.pwm = self.vec_to_pwn(sol)
-            s=[]
+            self.motifs.pwm = self.vec_to_pwm(sol)
+            s = []
             for seq in self.motifs.seqs:
-                p= self.motifs.mostProbableSeqEX(seq)
+                p = self.motifs.mostProbableSeq(seq)
                 s.append(p)
-            #TPC usar score multiplicativo sem atualizar PWM
+            ## TPC - ussar score multiplicativo sem atualizar a PWM ##
             fit = self.motifs.score(sol)
-
-
+            ind.setFitness(fit)
 
 def test1():
     ea = EAMotifsInt(100, 1000, 50, "exemploMotifs.txt")
@@ -82,10 +94,10 @@ def test1():
 
 
 def test2():
-    ea = EAMotifsReal(100, 2000, 50, "exemploMotifs.txt", 2)
+    ea = EAMotifsReal(100, 2000, 50, "exemploMotifs.txt")
     ea.run()
     ea.printBestSolution()
 
 
-test1()
-# test2()
+#test1()
+test2()
--- EvolAlgorithm.py
+++ EvolAlgorithm.py
@@ -4,38 +4,39 @@ from Popul import Popul
 class EvolAlgorithm:
 
     def __init__(self, popsize, numits, noffspring, indsize):
-        self.popsize = popsize
-        self.numits = numits
-        self.noffspring = noffspring
-        self.indsize = indsize
+        self.popsize = popsize#tamanho da pop
+        self.numits = numits # numero de iteracoes
+        self.noffspring = noffspring #numero de novos descendentes
+        self.indsize = indsize#tamanho dos individuos
 
-    def initPopul(self, indsize):
-        self.popul = Popul(self.popsize, indsize)
+    def initPopul(self, indsize):#geracao de uma pop inicial
+        self.popul = Popul(self.popsize, indsize)#tamanho da pop e dos ind
 
-    def evaluate(self, indivs):
-        for i in range(len(indivs)):
+    def evaluate(self, indivs):#avaliacao
+        for i in range(len(indivs)):#defenir a funcao de avaliacao para cada ind
             ind = indivs[i]
             fit = 0.0
-            for x in ind.getGenes():
-                if x == 1:
-                    fit += 1.0
-            ind.setFitness(fit)
+            for x in ind.getGenes():#para cada ind vai buscar a sus representacao (vetor de 0 e 1)
+                if x == 1:#percorre o vetor td
+                    fit += 1.0#adiciona
+            ind.setFitness(fit)#quando acaba atribui essa fitness ao individuo
         return None
 
-    def iteration(self):
-        parents = self.popul.selection(self.noffspring)
-        offspring = self.popul.recombination(parents, self.noffspring)
-        self.evaluate(offspring)
-        self.popul.reinsertion(offspring)
+    def iteration(self):#iteracao (estrutura do algoritmo evolucionario)
+        parents = self.popul.selection(self.noffspring)#selecao para reproducao
+        offspring = self.popul.recombination(parents, self.noffspring)#nova geracao atraves da recombinacao
+        self.evaluate(offspring)#avaliar a populacao
+        self.popul.reinsertion(offspring)#fazer a reinsercao (selecao dos individuos que vao constituir a populacao OU iteracao seguinte
+        #continuar ate atingir o criterio de paragem
 
     def run(self):
-        self.initPopul(self.indsize)
-        self.evaluate(self.popul.indivs)
-        self.bestsol = self.popul.bestSolution()
-        for i in range(self.numits+1):
-            self.iteration()
-            bs = self.popul.bestSolution()
-            if bs > self.bestsol:
+        self.initPopul(self.indsize)#criar a pop inicial
+        self.evaluate(self.popul.indivs)#avaliar essa pop
+        self.bestsol = self.popul.bestSolution()#solucao inicial
+        for i in range(self.numits+1):#numero de iteracoes +1 porque o range n inclui a ultima
+            self.iteration()#fazer a iteracao
+            bs = self.popul.bestSolution()#atualizar a solucao
+            if bs > self.bestsol:#avaliar a nova solucao
                 self.bestsol = bs
             print("Iteration:", i, " ", "Best: ", self.bestsol)
 
@@ -47,7 +48,10 @@ class EvolAlgorithm:
 def test():
     ea = EvolAlgorithm(100, 20, 50, 10)
     ea.run()
-
+def test1():
+    ea = EvolAlgorithm(100, 20, 50, 100)#mais complexo porque tem mais individuos (100)
+    ea.run()
 
 if __name__ == "__main__":
-    test()
+    #test()
+    test1()
